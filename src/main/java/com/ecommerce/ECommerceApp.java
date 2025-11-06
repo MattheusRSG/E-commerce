41b5e0e854eccd977dae9ac9845c5084ff7b6982
Charles <charles.aragao08@hotmail.com>
2025-11-06 20:52:03 -0300

    teste git

--- src/main/java/com/ecommerce/ECommerceApp.java
+++ src/main/java/com/ecommerce/ECommerceApp.java
@@ -6,5 +6,6 @@ import javax.swing.SwingUtilities;
 public class ECommerceApp {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {new LoginFrame().setVisible(true);});
+
     }
 }
